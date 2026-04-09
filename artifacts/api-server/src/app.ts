df8a0488fee3a6bd7574947991ea93e23e9e4fe2
rabbi333-sys <zakarieahasan56@gmail.com>
2026-04-10 00:45:17 +0600

    Refactor imports and update request/response types

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
-import express, { type Express } from "express";
+import express, { type Express, Request, Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import { pinoHttp } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -10,25 +10,24 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: Request) {
         return {
           id: req.id,
           method: req.method,
-          url: req.url?.split("?")[0],
+          url: req.url,
         };
       },
-      res(res) {
+      res(res: Response) {
         return {
           statusCode: res.statusCode,
         };
       },
     },
-  }),
+  })
 );
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", router);
 
 export default app;
